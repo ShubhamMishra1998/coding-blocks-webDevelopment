84073a810730e61c6b23f0280e753b850655f9d6
Shubham Mishra <sm544735@gmail.com>
2021-09-21 01:59:39 +0530

    assignment-5

---
+++ JavaScript/Lecture-3/JS-FUNCTIONS/app.js
@@ -0,0 +1,29 @@
+"use script";
+// function
+// function fun() {
+//   console.log("hello function");
+// }
+// fun();
+
+// // with argument
+// function add(a, b) {
+//   console.log(a + b);
+// }
+// add(2, 3);
+
+// function sub(a, b, c = 2) {
+//   console.log(a - b - c);
+// }
+// sub(2, 3);
+// sub(2, 3, 4); //override c
+
+// storig function in a variable
+
+// const f = function (a, b) {
+//   console.log("inside f");
+// };
+
+var x = 100;
+function fun() {
+  var y = 99;
+}
---
+++ JavaScript/Lecture-3/JSLOOPS/app.js
@@ -0,0 +1,13 @@
+"use script";
+// for loop
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+// *************************for of loop**********
+const a = [1, 2, 3, 4, 5];
+for (let x of a) {
+  console.log(x);
+}
+const person = {
+  nam,
+};
