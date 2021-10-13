e5f054fdb06c69da02de59612b46b91f23c269ac
Shubham Mishra <sm544735@gmail.com>
2021-10-14 01:56:30 +0530

    lecture2

---
+++ NodeJs/Lecture-2/Require&ModuleExports/index.js
@@ -0,0 +1,4 @@
+const { PI, square, add } = require("./math");
+console.log(PI);
+console.log(square(5));
+console.log(add(2, 3));
---
+++ NodeJs/Lecture-2/Require&ModuleExports/math.js
@@ -0,0 +1,9 @@
+const PI = 3.1456;
+const square = (num) => num * num;
+const add = (a, b) => a + b;
+
+module.exports = {
+  PI,
+  square,
+  add,
+};
