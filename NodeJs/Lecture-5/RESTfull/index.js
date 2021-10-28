2d1bfcc64f6a1cf623e78793f861c55849370474
Shubham Mishra <sm544735@gmail.com>
2021-10-28 23:16:51 +0530

    lecture-5

--- NodeJs/Lecture-5/RESTfull/index.js
+++ NodeJs/Lecture-5/RESTfull/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
+const methodOverride = require("method-override");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
 app.use(express.static(path.join(__dirname, "public")));
-const { v4: uuid } = require("uuid");
 app.use(express.urlencoded({ extended: true }));
-const comments = [
+app.use(methodOverride("_method"));
+
+let comments = [
   {
     id: uuid(),
     username: "Sabeel",
@@ -27,24 +32,34 @@ const comments = [
 app.get("/", (req, res) => {
   res.send("connected");
 });
-// get the form for new comment
-app.get("/comments/new", (req, res) => {
-  res.render("new");
-});
-// list all the comments
+
+// List all the comments
 app.get("/comments", (req, res) => {
   res.render("index", { comments });
 });
 
-// displaying particular comment
+// Get the form for new comment
+app.get("/comments/new", (req, res) => {
+  res.render("new");
+});
+
+// Creating a New Comment
+app.post("/comments", (req, res) => {
+  const { username, text } = req.body;
+
+  comments.push({ username, text, id: uuid() });
+
+  res.redirect("/comments");
+});
+
+// Displaying Particular Comment
 app.get("/comments/:commentid", (req, res) => {
-  console.log(req.params);
   const { commentid } = req.params;
   const foundComment = comments.find((comment) => comment.id === commentid);
-  console.log(foundComment);
   res.render("show", { comment: foundComment });
 });
 
+// Get the edit form prefilled with the current data
 app.get("/comments/:commentid/edit", (req, res) => {
   const { commentid } = req.params;
 
@@ -53,13 +68,28 @@ app.get("/comments/:commentid/edit", (req, res) => {
   res.render("edit", { comment: foundComment });
 });
 
-// creating a new comment
-app.post("/comments", (req, res) => {
-  const { username, text } = req.body;
-  comments.push({ username, text, id: uuid() });
+// Updating the comment Text
+app.patch("/comments/:commentid", (req, res) => {
+  const { commentid } = req.params;
+  const foundComment = comments.find((comment) => comment.id === commentid);
+
+  const { text } = req.body;
+
+  foundComment.text = text;
+
+  res.redirect("/comments");
+});
+
+app.delete("/comments/:commentid", (req, res) => {
+  const { commentid } = req.params;
+  const newCommentsArray = comments.filter(
+    (comment) => comment.id !== commentid
+  );
+  comments = newCommentsArray;
+
   res.redirect("/comments");
 });
 
 app.listen(3000, () => {
-  console.log("server listining at port 3000");
+  console.log("server running at port 3000");
 });
