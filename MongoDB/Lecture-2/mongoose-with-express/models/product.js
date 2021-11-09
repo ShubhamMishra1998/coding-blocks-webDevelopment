3a00f4e7a9487883bf2017a939c2ed0116008cd5
Shubham Mishra <sm544735@gmail.com>
2021-11-09 19:03:40 +0530

    mongoose

---
+++ MongoDB/Lecture-2/mongoose-with-express/models/product.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+// define schema
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  price: {
+    type: Number,
+    min: 0,
+  },
+  desc: {
+    type: String,
+    trim: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
